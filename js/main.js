7a8b7e9da35e9f1e28a82649078de6e7dfe101a7
anushahasti <anusha.hasthi@gmail.com>
2021-01-28 23:51:17 -0700

    19
    19th homework

--- js/main.js
+++ js/main.js
@@ -4,3 +4,9 @@ let navBarToggle = document.getElementById("js-navbar-toggle");
 navBarToggle.addEventListener("click", function() {
   mainNav.classList.toggle("active");
 });
+
+$(document).ready(function(){
+  $("bounceMe").click(function(){
+    $("#myimage").css("color", "red").slideUp(2000);
+  });
+});
